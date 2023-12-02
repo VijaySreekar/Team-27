455f8744ad1d82adf7542c1527355bdbb7f0e86b
Daniel Iqbal <danieliqbalbusiness@gmail.com>
2023-12-02 07:38:16 +0000

    Work

---
+++ Math.java
@@ -0,0 +1,89 @@
+package math;
+
+public class Math {
+	
+	public Math() {}
+	
+	/*
+	 * Developer: Daniel Iqbal
+	 * University ID: 220119832
+	 * Function: Max method
+	 */
+	
+	public int Max(int a, int b) {
+		if (a < b) {
+			return b;
+		} else if (a > b) {
+			return a;
+		} else {
+			return null;
+		}
+	}
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// Min method
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// Add method
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// Sub Method
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// Multi Method
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// Divid Method
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// Power Method
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// Mod method
+	
+	/*
+	 * Developer:
+	 * University ID: 
+	 * Function: 
+	 */
+	
+	// SumTest() method
+	
+
+
+}
---
+++ profile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php 
+    session_start();
+    include("connectdb.php");
+    ?>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Your Profile</title>
+  </head>
+  <body>
+    <header id="main-header">
+     <nav>
+       <!-- navigation bar -->
+     </nav>
+    </header>
+    <main>
+      <?php
+        if(isset($_SESSION['username'])){
+          echo "<h3>Welcome, " . $_SESSION['username'] . "!</h3>";
+        } else {
+          echo "<h3>You aren't logged in.</h3>";
+        }
+      ?>
+    </main>
+  </body>
+</html>
