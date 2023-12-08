f9eedf7c71a8623fdeb278df765a0a6dfbb989aa
EhsaanJaan <redehsaan@gmail.com>
2023-12-08 15:14:30 +0000

    add nav bar

--- contactus.php
+++ contactus.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
+<?php include 'nav.php'; ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,8 +19,6 @@
     </style>
 </head>
 <body>
-
-<?php include 'nav.php'; ?>
 
 <div class="contact-form">
     <h2>Contact Us</h2>
