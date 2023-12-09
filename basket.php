ef1f42f1c3f2908226066fbfa7bc73e7d0f24955
Hanifa78 <148325572+Hanifa78@users.noreply.github.com>
2023-12-09 20:38:11 +0000

    basket

--- basket.php
+++ basket.php
@@ -1,24 +1,24 @@
 <?php
-
-include("connectdb.php");
-
-// Fetch basket items from the database
-$basketItemsQuery = $mysqli->query("SELECT * FROM basket");
-if (!$basketItemsQuery) {
-    die('Error in SQL query: ' . $mysqli->error);
-}
-
-$basketItems = $basketItemsQuery->fetch_all(MYSQLI_ASSOC);
+// Sample basket items (you can replace this with your logic to fetch items from a database, for example)
+$basketItems = [
+    ['Product 1', 20.00, 2],
+    ['Product 2', 15.00, 1],
+    ['id' => 1, 'name' => 'Nike AirForce', 'price' => 70, 'quantity' => 2],
+    ['id' => 2, 'name' => 'New Balance', 'price' => 30, 'quantity' => 1],
+    
+];
 
 // Function to calculate the total price
 function calculateTotal($basketItems) {
     $total = 0;
     foreach ($basketItems as $item) {
+        $total += $item[1] * $item[2];
         $total += $item['price'] * $item['quantity'];
     }
     return $total;
 }
-?>
+
+@@ -17,17 +18,19 @@ function calculateTotal($basketItems) {
 
 <!DOCTYPE html>
 <html lang="en">
@@ -34,41 +34,32 @@ function calculateTotal($basketItems) {
     <nav class="navbar">
         <div class="navbar-left">
             <div class="logo">
+                <img src="Images/YourLogo.png" alt="Your Logo">
                 <img src="Images/Treakers%20Logo.png" alt="Your Logo">
             </div>
         </div>
         <div class="navbar-center">
-            <ul class="nav-links">
-                <li><a href="#">Home</a></li>
-                <li class="center"><a href="#">Products</a></li>
-                <li class="upward"><a href="#">About</a></li>
-                <li class="forward"><a href="#">Contact Us</a></li>
-            </ul>
-        </div>
-        <div class="navbar-right">
-            <div class="buttons">
-                <button class="basket">
-                    <i class="fas fa-shopping-basket"></i> Basket
-                </button>
-            </div>
-            <div class="search-bar">
-                <input type="text" placeholder="Search">
-                <button class="search-button"><i class="fas fa-search"></i></button>
-            </div>
-        </div>
-    </nav>
 
-    <div class="basket-container">
-        <div class="basket-heading">
-            <h2>Your Basket</h2>
+
+@@ -57,33 +60,79 @@ function calculateTotal($basketItems) {
         </div>
         <div class="basket-item-heading">
             <h3>Item</h3>
+            <h3>Price</h3>
             <h3>Quantity</h3>
             <h3>Total</h3>
             <h3>Remove</h3>
         </div>
 
+        <!-- Dynamically generate basket items -->
+        <?php foreach ($basketItems as $item): ?>
+            <div class="basket-item">
+                <div class="basket-item-detail"><?= $item[0] ?></div>
+                <div class="basket-item-detail">$<?= number_format($item[1], 2) ?></div>
+                <div class="basket-item-detail"><?= $item[2] ?></div>
+                <div class="basket-item-detail">$<?= number_format($item[1] * $item[2], 2) ?></div>
+            </div>
+        <?php endforeach; ?>
        
         <?php
         foreach ($basketItems as $item) {
@@ -81,11 +72,19 @@ function calculateTotal($basketItems) {
         }
         ?>
 
+        <!-- Total price -->
+        <div class="total-price">Total Price: $<?= number_format(calculateTotal($basketItems), 2) ?></div>
         <div class="total-price">Total Price: £<?= number_format(calculateTotal($basketItems), 2) ?></div>
 
+        <!-- Pay Now button -->
         <form action="payment.php" method="post">
             <button type="submit">Pay Now</button>
         </form>
+        
+<!-- Footer -->
+<footer class="footer">
+    <p>&copy; 2023 Your Company. All rights reserved.</p>
+</footer>
     </div>
 
     <script>
@@ -138,5 +137,6 @@ function calculateTotal($basketItems) {
 </script>
 
 </body>
+</html>
 
 </html>
