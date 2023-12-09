1f0f976ee0f845738f4416dbad3ed3ffdff6f8b5
Samuel Krampah <210141205@aston.ac.uk>
2023-12-09 21:06:47 +0000

    HomePage Update
    Preset Nav bar added, everything should be in the right place

--- Home Page/HomePage.php
+++ Home Page/HomePage.php
@@ -10,18 +10,62 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   </head>
   <body>
-  <!--Navigation Bar-->
-  <?php include("Nav Bar\\nav.php"); ?>
-<!--Text on picture--> 
+  
+    <!--Navigation Bar-->
+  
+    <body>
+      <nav class="navbar navbar-fixed-top">
+          <div class="navbar-left">
+              <div class="logo">
+                  <img src="../Images/Treakers%20Logo.png" alt="Company Logo">
+              </div>
+          </div>
+          <div class="navbar-center">
+              <ul class="nav-links">
+                  <li><a href="../index.html">Home</a></li>
+                  <li class="center"><a href="../products-page.php">Products</a></li>
+                  <li class="upward"><a href="../aboutus.php">About</a></li>
+                  <li class="forward"><a href="../contacatus.php">Contact Us</a></li>
+              </ul>
+          </div>
+          <div class="navbar-right">
+              <div class="buttons">
+                  <?php
+                  // Check if the user is logged in and has a valid session
+                  if (isset($_SESSION['user_id']) && isset($_SESSION['name'])) {
+                      // Display the username with a class
+                      echo '<span class="username"><i class="fas fa-user"></i> ' . $_SESSION['name'] . '</span>';
+                  } else {
+                      // Display the "Login/Signup" button with a class
+                      echo '<button class="login-signup-btn"><i class="fas fa-user"></i> Login/Signup</button>';
+                  }
+                  ?>
+                  <button class="basket">
+                      <i class="fas fa-shopping-basket"></i>
+                  </button>
+              </div>
+              <div class="search-bar">
+                  <input type="text" placeholder="Search">
+                  <button class="search-button"><i class="fas fa-search"></i></button>
+              </div>
+          </div>
+      </nav>
+  </body>
+
+    <!--Text on picture--> 
+    
     <section>
       <div class="bg" style="background-image: url('Sneaker Wallpaper.jpeg')"></div>
       <h1>Treakers</h1>
       <p>Welcomes you to the family</p>
     </section>
-<!--Trending items section-->
-  <body>
+
+    <!--Trending items section-->
+ 
+    <body>
     <section class="trending-section">
       <h1>Trending Items</h1>
           <!-- Add other swiper-slide elements as needed -->
@@ -65,8 +109,10 @@
     </section>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
   </body>
-<!--Trending items section-->
-    <section>
+
+  <!--Trending items section-->
+    
+  <section>
       <div class="bg" style="background-image: url('Air Jordan 11.jpg.avif')"></div>
       <h1>Join us today</h1>
   </section>
@@ -85,7 +131,9 @@
     ></script>
 
     <script src="HomePage.js"></script>
-<!--Footer Section-->
+
+    <!--Footer Section-->
+    
     <footer>
       <p2>&copy; 2023 Treakers. All rights reserved.</p2>
     </footer>
--- Nav Bar/nav.php
+++ Nav Bar/nav.php
@@ -4,7 +4,7 @@
         <link rel="stylesheet" href="../nav.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
     </head>
     <body>
         <nav class="navbar navbar-fixed-top">
