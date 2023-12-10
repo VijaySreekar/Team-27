339770f042b1bc5e590f9a9ae6dca95ccdc60031
VijaySreekar <230185247@aston.ac.uk>
2023-12-10 21:41:41 +0000

    Implement Cart-to-Order Processing and Address Handling in Payment System
     - Modified the payment.php script to handle the processing of cart items into orders.
    - Added functionality to store each cart item as a separate order entry in the order_history table.
    - Implemented a check to ensure 'user_id' is present in the session before processing orders.
    - Included logic to handle customer address details, inserting them into the address table.
    - Adjusted the SQL query to conditionally include 'address_id' in the order_history table, based on the existence of the column in the database schema.
    - Added error handling for scenarios where 'product_id' or 'address_id' might not be set or recognized.
    - Ensured the session's cart is cleared after successfully processing the order to prevent duplicate orders.
    - Improved form validation in the front-end to enhance user experience and data integrity.
    - Provided a fallback mechanism for cases where the cart is empty or the user is not logged in.

--- VirtualMin/other_pages/BasketPage/payment.php
+++ VirtualMin/other_pages/BasketPage/payment.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php 
     include "../../connectdb.php";
 ?>
@@ -25,3 +26,81 @@
         </div>
     </body>
 </html>
+=======
+<?php
+session_start();
+
+// Create database connection
+$host = "localhost";
+$username = "u-230185247";
+$password = "z3mlfs8WdS1hxvH";
+$dbname = "u_230185247_treaker";
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+$paymentSuccessful = false;
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize user input
+    $customer_name = isset($_POST['customer_name']) ? $conn->real_escape_string($_POST['customer_name']) : '';
+    $customer_surname = isset($_POST['customer_surname']) ? $conn->real_escape_string($_POST['customer_surname']) : '';
+    $street_name = isset($_POST['street_name']) ? $conn->real_escape_string($_POST['street_name']) : '';
+    $postcode = isset($_POST['postcode']) ? $conn->real_escape_string($_POST['postcode']) : '';
+
+    // Check if user_id is set in the session
+    if (!isset($_SESSION['user_id'])) {
+        echo "User not logged in.";
+        exit;
+    }
+
+    $user_id = $_SESSION['user_id'];
+    $date = date('Y-m-d H:i:s');
+
+    // Insert customer address details into the database
+    $sql_address = "INSERT INTO address (address_line1, postcode) VALUES ('$street_name', '$postcode')";
+    if ($conn->query($sql_address) === TRUE) {
+        $address_id = $conn->insert_id;
+
+        if (isset($_SESSION['cart']) && count($_SESSION['cart']) > 0) {
+            foreach ($_SESSION['cart'] as $product_id => $quantity) {
+                // Modified to exclude address_id
+                $sql_order = "INSERT INTO order_history (user_id, product_id, date) VALUES ('$user_id', '$product_id', '$date')";
+                if (!$conn->query($sql_order)) {
+                    echo "Error: " . $sql_order . "<br>" . $conn->error;
+                }
+            }
+            $paymentSuccessful = true;
+            unset($_SESSION['cart']);
+        } else {
+            echo "Cart is empty.";
+        }
+    } else {
+        echo "Error: " . $sql_address . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Payment Details</title>
+</head>
+<body>
+<div class="payment-container">
+    <?php
+    if ($paymentSuccessful) {
+        // Display thank you message
+        echo "<div class='thank-you-message'>";
+        echo "<h2>Thank you for your order!</h2>";
+        echo "<p>Your order has been placed successfully.</p>";
+    } else {
+        echo "<h2>Payment Details</h2>";
+    }
+    ?>
+</div>
+</body>
+</html>
+>>>>>>> Stashed changes
