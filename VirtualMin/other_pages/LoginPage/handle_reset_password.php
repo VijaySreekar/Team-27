faedd08bce238710911c29f71fbbf48af4f66034
VijaySreekar <230185247@aston.ac.uk>
2023-12-10 06:37:43 +0000

    Implementing Forgot Password Feature
    Added a 'Forgot Password?' link to the login form in login_page.php, directing users to a new page for password recovery.
    
    Creating forgot_password.php with a form for resetting the password, including fields for email, new password, and confirm password.
    
    Developing backend logic in handle_reset_password.php for password reset, including input validation, password hashing, and updating the user's password in the database.

---
+++ VirtualMin/other_pages/LoginPage/handle_reset_password.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to the login page or any other appropriate action
+    header("Location: login.php"); // Replace "login.php" with your login page URL
+    exit();
+}
+
+
+$host = "localhost";
+$username = "u-230185247";
+$password = "z3mlfs8WdS1hxvH";
+$dbname = "u_230185247_treaker";
+
+// Create database connection
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Include database connection file
+
+    // Collect and sanitize input data
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $new_password = $_POST['new_password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    // Check if the new password and confirm password match
+    if ($new_password != $confirm_password) {
+        echo "Passwords do not match.";
+        exit;
+    }
+
+    // Check if the email exists in the database
+    $sql = "SELECT * FROM user WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // Email exists, proceed with updating the password
+
+        // Hash the new password
+        $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+
+        // Update the password in the database
+        $update_sql = "UPDATE user SET passwordhash = '$hashed_password' WHERE email = '$email'";
+        if (mysqli_query($conn, $update_sql)) {
+            echo "Password successfully changed.";
+        } else {
+            echo "Error updating password: " . mysqli_error($conn);
+        }
+    } else {
+        // Email does not exist
+        echo "No account found with that email address.";
+    }
+
+    // Close database connection
+    mysqli_close($conn);
+}
+?>
