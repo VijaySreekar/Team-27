87067b9a7fb9ad9c60e8232ac51d59ff8b7e6dc9
EhsaanJaan <redehsaan@gmail.com>
2023-12-10 06:20:04 +0000

    css shanges

--- aboutus.php
+++ aboutus.php
@@ -19,10 +19,10 @@ include("Nav Bar\\nav.php");
 <div>
     <h1>About Us</h1>
     <p>
-        Welcome to Treakers! I need to think of something to put in this goofy section.
+        Welcome to Treakers! We are an innovative Sneaker company keen to provide the world with easy access to trainers and sneakers. Whether you're a dedicated sneakerhead or just looking for a stylish pair of kicks, Treakers has you covered.
     </p>
     <p>
-        Second paragraph for extra info. Web development isn't fun.
+    We believe that sneakers are more than just shoes; they are a statement of style, individuality, and self-expression. Treakers is far more than just an online store; it's a community of sneaker enthusiasts brought together by a shared love for footwear. Join us on this exciting journey, and embrace your urban sole. here at Treakers.
     </p>
     <a href="contactus.php">Contact Us</a>
 </div>
