39036db474a4525721e2293c0c361ce591845f32
coff-ee <safaa.imran7@gmail.com>
2024-03-21 00:26:41 +0000

    review update
    incomplete

--- VirtualMin/other_pages/ReviewPage/review.php
+++ VirtualMin/other_pages/ReviewPage/review.php
@@ -1,22 +1,85 @@
 <?php
-    session_start(); // Start the session
+    //session_start(); // Start the session
     include("../../connectdb.php");
-    include("../../edit_user.php");
+
+    if(isset($_GET['pid'])){
+        require_once "../../connectdb.php";
+        
+        $pid = mysqli_real_escape_string($mysqli, $_GET['pid']);
+        $uid = $_SESSION['user_id'];
+
+    	$sql = "SELECT * 
+        		FROM product 
+                WHERE product_id='$pid'";
+        $result = mysqli_query($mysqli, $sql);
+        $row = mysqli_fetch_array($result);    
+    }
+
+    if(isset($_POST['submit'])){
+    	require_once "../../connectdb.php";
+        $rating = $_POST['rating'];
+        $comment = $_POST['comment'];
+		
+    	$rating = mysql_real_escape_string($rating);
+		$comment  = mysql_real_escape_string($comment);
+    	$uid = mysql_real_escape_string($uid);
+		$pid = mysql_real_escape_string($pid);
+
+         $sql = "INSERT INTO user_review
+         		 (user_id, product_id, rating, comment)
+                 VALUES
+                 ('$uid', '$pid', '$rating', '$comment')";
+    	 
+         $result = mysqli_query($mysqli, $sql);
+        //$stmt = $mysqli->stmt_init();
+
+        // if(!$stmt->prepare($sql)){
+        //     die("SQL error:".$mysqli->error);
+        // }
+
+        // $stmt->bind_param("iiis",
+        //                    $_SESSION['user_id'],
+        //                    $pid,
+        //                    $_POST["rating"],
+        //                    $_POST["comment"]);
+        // $stmt->execute();
+
+        //header("Location:../../profile.php");
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Leave A Review</title>
-    <link rel="stylesheet" href="review.css">
-    <link rel="stylesheet" href="../NavBar/nav.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
-    <?php 
-        include '../NavBar/nav.php';
-    ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Leave A Review</title>
+        <link rel="stylesheet" href="review.css">
+        <link rel="stylesheet" href="../NavBar/nav.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+    <body>
+        <?php 
+            include '../NavBar/nav.php';
+        ?>
+        <div class ="container">
+            <h2>Leave a review!</h2>
+        </div>
+        <div class ="container">
+            <h2><?php echo $row['name']?></h2>
+            <h3><?php echo $row['price']?></h3>
+        </div>
+        <main id ="mainform">
+            <form method="POST">
+                <label>Rating:</label>
+                <input type="text" name="rating" id="rating" required>
+                <br/><br/>
+                <label>Comment:</label>
+                <textarea rows="7" cols="55" name="comment" id="comment"></textarea>
+                <br/><br/>
+                <button type="submit" class="button" name="submit">Submit Review</button>
+            </form>
+        </main>
+    </body>
+</html>
