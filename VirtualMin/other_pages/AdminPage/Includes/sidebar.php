638c65f390d11c61ce408197f5766d3adaf5cd8e
Hanifa78 <148325572+Hanifa78@users.noreply.github.com>
2024-03-22 00:56:22 +0000

    Stock updates

--- VirtualMin/other_pages/AdminPage/Includes/sidebar.php
+++ VirtualMin/other_pages/AdminPage/Includes/sidebar.php
@@ -51,7 +51,14 @@
                     <span class="nav-link-text ms-1">Add Products</span>
                 </a>
             </li>
-
+            <li class="nav-item">
+                <a class="nav-link text-white <?= $page == "stock.php"?'active bg-gradient-primary':'' ?>" href="stock.php">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">table_view</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Stock</span>
+                </a>
+            </li>
 
         </ul>
     </div>
