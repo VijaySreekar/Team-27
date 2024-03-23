108ba293a37feee0e8c2492118014f15c24620f1
VijaySreekar <230185247@aston.ac.uk>
2024-03-23 01:32:32 +0000

    Enhancements  in Order Management System
    Added a detailed order viewing page for admin, complete with delivery details, order items, and the ability to update order status.
    
    Introduced a centralized order listing page with functionality to navigate to individual order details or the order history.

---
+++ VirtualMin/other_pages/AdminPage/allorders.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+include 'Includes/header.php';
+include 'AllFunctions/myfunctions.php';
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <span class="fw-bold fs-2">All Orders</span>
+                    <a href="order_history.php" class="btn btn-info float-end">Order History</a>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                        <tr>
+                            <th>OrderID</th>
+                            <th>User</th>
+                            <th>Tracking No</th>
+                            <th>Price</th>
+                            <th>Order Date</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $orders = getAllOrders();
+
+                        if(mysqli_num_rows($orders) > 0)
+                        {
+                            foreach ($orders as $order)
+                            {
+                                ?>
+                                <tr>
+                                    <td><?= $order['id']; ?></td>
+                                    <td><?= $order['name']; ?></td>
+                                    <td><?= $order['tracking_no']; ?></td>
+                                    <td><?= $order['total_price']; ?></td>
+                                    <td><?= $order['created_at']; ?></td>
+                                    <td><a href="view_order_admin.php?t=<?= $order['tracking_no']; ?>" class="btn btn-primary">View Details</a></td>
+
+
+                                </tr>
+                                <?php
+
+
+                            }
+                        }else
+                        {
+                            ?>
+                            <tr>
+                                <td colspan="5">No Orders Yet</td>
+                            </tr>
+                            <?php
+
+                        }
+                        ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'Includes/Footer.php'; ?>
+
+
---
+++ VirtualMin/other_pages/AdminPage/view_order_admin.php
@@ -0,0 +1,166 @@
+<?php
+session_start();
+include 'Includes/header.php';
+include 'AllFunctions/myfunctions.php';
+
+$host = "localhost";
+$username = "u-230185247";
+$password = "z3mlfs8WdS1hxvH";
+$dbname = "u_230185247_treaker";
+
+// Create database connection
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+if(isset($_GET['t']))
+{
+    $tracking_no = $_GET['t'];
+
+    $order_details = AdmincheckTrackingNo($tracking_no);
+    if(mysqli_num_rows($order_details) == 0)
+    {
+        ?>
+        <h4>No Orderss Found</h4>
+        <?php
+        die();
+    }
+
+}
+else
+{
+    ?>
+    <h4>No Order Found</h4>
+    <?php
+    die();
+}
+
+$order_data = mysqli_fetch_array($order_details);
+?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mt-4">
+                <div class="card-header bg-primary">
+                    <span class="text-white fs-2">View Order</span>
+                    <a href="allorders.php" class="btn btn-dark-blue float-end">Back</a>
+                </div>
+                <div class="card-body">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <h4>Delivery Details</h4>
+                                        <div class="row">
+                                             <div class="col-md-12">
+                                                 <label class="fw-bold fs-5 mb-0">Name</label>
+                                                 <div class="border p-1">
+                                                     <?= $order_data['name']; ?>
+                                                 </div>
+                                             </div>
+                                             <div class="col-md-12">
+                                                <label class="fw-bold fs-5 mb-0">Email</label>
+                                                <div class="border p-1">
+                                                    <?= $order_data['email']; ?>
+                                                </div>
+                                             </div>
+                                             <div class="col-md-12">
+                                                <label class="fw-bold fs-5 mb-0">Phone</label>
+                                                <div class="border p-1">
+                                                    <?= $order_data['phone']; ?>
+                                                </div>
+                                             </div>
+                                            <div class="col-md-12">
+                                                <label class="fw-bold fs-5 mb-0">Tracking Number:</label>
+                                                <div class="border p-1">
+                                                    <?= $order_data['tracking_no']; ?>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <label class="fw-bold fs-5 mb-0">Address</label>
+                                                <div class="border p-1">
+                                                    <?= $order_data['address']; ?>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <label class="fw-bold fs-5 mb-0">Pin Code</label>
+                                                <div class="border p-1">
+                                                    <?= $order_data['pincode']; ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <h4>Order Details</h4>
+
+                                        <table class="table text-center">
+                                            <thead>
+                                                <tr>
+                                                    <th>Product</th>
+                                                    <th>Price</th>
+                                                    <th>Quantity</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                                <?php
+
+                                                $order_query = "SELECT o.id as oid, o.tracking_no, o.user_id, oi.*, oi.quantity as order_quantity, p.* FROM orders o, order_items oi, product p
+                                                       WHERE oi.order_id = o.id AND p.product_id = oi.product_id AND o.tracking_no = '$tracking_no'";
+                                                $order_items = mysqli_query($conn, $order_query);
+
+                                                if(mysqli_num_rows($order_items) > 0)
+                                                {
+                                                    foreach ($order_items as $item) {
+
+                                                    }
+                                                        ?>
+                                                            <tr>
+                                                                <td class="align-middle">
+                                                                    <img src="../AdminPage/<?= $item['image']; ?>" alt="<?= $item['name']; ?>" style="width: 50px;">
+                                                                    <?= $item['name']; ?>
+                                                                </td>
+                                                                <td class="align-middle">
+                                                                    <?= $item['price']; ?>
+                                                                </td>
+                                                                <td class="align-middle">
+                                                                    <?= $item['order_quantity']; ?>
+                                                                </td>
+                                                            </tr>
+
+                                                    <?php
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                        <hr>
+                                        <h4>Total Price: <span class="float-end">£<?= $order_data['total_price']; ?></span></h4>
+
+                                        <label class="fw-bold fs-6 mb-1 mt-2">Payment Mode:</label>
+                                        <div class="border p-3">
+                                            <?= $order_data['payment_mode']; ?>
+                                        </div>
+                                        <label class="fw-bold fs-6 mb-1 mt-2">Order Status:</label>
+                                        <div class="p-2">
+                                            <form action="add_category_code.php" method="POST">
+                                                <input type="hidden" name="tracking_no" value="<?= $order_data['tracking_no']; ?>">
+                                                <select name="order_status" class="form-select">
+                                                    <option value="0" <?= $order_data['status'] == 0?"selected":"" ?>>Order Placed</option>
+                                                    <option value="1" <?= $order_data['status'] == 1?"selected":"" ?>>Order Shipped</option>
+                                                    <option value="2" <?= $order_data['status'] == 2?"selected":"" ?>>Order Delivered</option>
+                                                    <option value="3" <?= $order_data['status'] == 3?"selected":"" ?>>Order Cancelled</option>
+                                                </select>
+                                                <button type="submit" name='updateOrderButton' class="btn btn-primary mt-2">Update Status</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
