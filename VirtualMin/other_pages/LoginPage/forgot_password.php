0e488c62a45b8fd98f1ba978c35c4dc587f6e2a6
VijaySreekar <230185247@aston.ac.uk>
2024-03-23 20:18:08 +0000

    Forgot Password UI Change

--- VirtualMin/other_pages/LoginPage/forgot_password.php
+++ VirtualMin/other_pages/LoginPage/forgot_password.php
@@ -5,101 +5,79 @@ session_start(); // Start the session
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-    <meta charset="UTF-8">
-    <title>Login/Register</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Category Page</title>
+
     <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Fonts and icons -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../NavBar_Footer/nav.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Truncleta:wght@400&display=swap">
+    <link rel="stylesheet" href="../NavBar_Footer/new_nav.css">
 
+    <!-- Nucleo Icons -->
+    <link href="../AdminPage/assets/nucleo-icons.css" rel="stylesheet" />
+    <link href="../AdminPage/assets/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../AdminPage/assets/material-dashboard.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
 
+    <!-- Alertify JS -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
+
+    <link rel="stylesheet" href="../NavBar_Footer/new_nav.css">
 </head>
 
-<body>
-<?php
-include '../NavBar_Footer/nav.php';
-?>
-<div class="container">
-    <input type="checkbox" id="change">
-    <div class="cover">
-        <div class="login">
-            <img class="backImg" src="../../Images/Shoe%201%20(Login).png" alt="">
-            <div class="text">
-                <span class="slogan-1">Start your Adventure <br> Here</span>
-                <span class="slogan-2">Login and Discover!</span>
+<body class="g-sidenav-show bg-gray-200">
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <?php include '../NavBar_Footer/new_nav.php'; ?>
+    <div class="container mt-7 mb-9">
+        <input type="checkbox" id="change" style="display: none;">
+        <div class="cover">
+            <div class="login">
+                <img class="backImg" src="../../Images/Shoe%201%20(Login).png" alt="" style="object-fit: cover; width: 100%; height: 100%;"></div>
+            <div class="register" style="transform: rotateY(180deg); position: absolute; width: 100%; height: 100%;">
+                <img class="backImg" src="../../Images/Shoe%202%20(Registration).png" alt="" style="object-fit: cover; width: 100%; height: 100%;">
             </div>
         </div>
-        <div class="register">
-            <img class="backImg" src="../../Images/Shoe%202%20(Registration).png" alt="">
-        </div>
-    </div>
-    <div class="content">
-            <div class="passwordreset_form">
-                <div class="title">Password Recovery</div>
-                <form id="passwordreset-form" action="handle_reset_password.php" method="post">
-                    <div class="input-boxes">
-                        <div class="input-box">
-                            <i class="fas fa-user"></i>
-                            <input type="text" name="email" placeholder="Enter your Email" required>
+        <div class="loginregister_form pt-4" style="background: rgba(251,254,249,0);">
+            <div class="content" style="display: flex; justify-content: space-between;">
+                <div class="passwordreset-form">
+                    <div class="title" style="font-size: 24px; font-weight: 500; color: #333;">Password Recovery</div>
+                    <form id="passwordreset-form" action="handle_reset_password.php" method="post">
+                        <div class="input-boxes" style="margin-top: 30px;">
+                            <div class="input-box" style="display: flex; align-items: center; position: relative;">
+                                <i class="fas fa-user" style="position: absolute; color: #0c6291; font-size: 17px;"></i>
+                                <input type="text" placeholder="Enter your email" name="email" required style="padding-left: 30px; border-bottom: 2px solid rgba(0,0,0,0.2);">
+                            </div>
+                            <div class="input-box">
+                                <i class="fas fa-lock" style="position: absolute; color: #0c6291; font-size: 17px;"></i>
+                                <input type="password" name="new_password" placeholder="New password" required style="padding-left: 30px; border-bottom: 2px solid rgba(0,0,0,0.2);">
+                            </div>
+                            <div class="input-box">
+                                <i class="fas fa-lock" style="position: absolute; color: #0c6291; font-size: 17px;"></i>
+                                <input type="password" name="confirm_password" placeholder="Confirm password" required style="padding-left: 30px; border-bottom: 2px solid rgba(0,0,0,0.2);">
+                            </div>
+                            <div class="button input-box" style="margin-top: 40px;">
+                                <input type="submit" value="Reset Password" style="color: #fff; background: #0c6291; border-radius: 6px; cursor: pointer;">
+                            </div>
                         </div>
-                        <div class="input-box">
-                            <i class="fas fa-envelope"></i>
-                            <input type="text" name="new_password"  placeholder="New Password" required>
-                        </div>
-                        <div class="input-box">
-                            <i class="fas fa-lock"></i>
-                            <input type="password" name="confirm_password"  placeholder="Confirm Password" required>
-                        </div>
-                        <div class="button input-box">
-                            <input type="submit" value="Reset Password">
-                        </div>
-                    </div>
-                </form>
-                <div id="signup-error-message" style="color:red;"></div>
-            </div>
-    </div>
-</div>
-<footer class="footer">
-    <div class="fcontainer">
-        <div class="row">
-            <div class="footer-col">
-                <h4>Treakers</h4>
-                <ul>
-                    <li><a href="#">about us</a></li>
-                    <li><a href="#">our products</a></li>
-                    <li><a href="#">privacy policy</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h4>get help</h4>
-                <ul>
-                    <li><a href="#">FAQ</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">returns</a></li>
-                    <li><a href="#">Basket</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h4>online shop</h4>
-                <ul>
-                    <li><a href="#">Sneakers</a></li>
-                    <li><a href="#">Trainers</a></li>
-                </ul>
-            </div>
-            <div class="footer-col">
-                <h4>follow us</h4>
-                <div class="social-links">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </form>
+                    <div id="error-message" style="color:red;"></div>
                 </div>
             </div>
         </div>
     </div>
-</footer>
+    <?php include '../NavBar_Footer/footer.html'; ?>
+</main>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
     $(document).ready(function(){
@@ -151,8 +129,6 @@ include '../NavBar_Footer/nav.php';
         });
     });
 </script>
-
-
 </body>
 </html>
 
